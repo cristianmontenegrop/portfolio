a1026dd85bb3a1201aeca097154828bca80cdb46
Cris M <cristian.montenegro.p@gmail.com>
2020-11-27 19:56:19 -0800

    style: app.js code cleaning

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,28 +1,23 @@
-import { Controller, Scene } from 'react-scrollmagic';
-
-
-$(document).ready(function () {
-
+$(document).ready(() => {
   const controller = new ScrollMagic.Controller({
     globalSceneOptions: {
-      duration: 1050
-    }
+      duration: 1050,
+    },
   });
 
-  const portScene = new ScrollMagic.Scene({
-      triggerElement: "#portfolio"
-    })
-    .setClassToggle("#port-nav", "highlight") // add class toggle
+  new ScrollMagic.Scene({
+    triggerElement: '#portfolio',
+  })
+    .setClassToggle('#port-nav', 'highlight') // add class toggle
     .addTo(controller);
-  const aboutScene = new ScrollMagic.Scene({
-      triggerElement: "#about-me"
-    })
-    .setClassToggle("#about-nav", "highlight") // add class toggle
+  new ScrollMagic.Scene({
+    triggerElement: '#about-me',
+  })
+    .setClassToggle('#about-nav', 'highlight') // add class toggle
     .addTo(controller);
-  const contactScene = new ScrollMagic.Scene({
-      triggerElement: "#contact"
-    })
-    .setClassToggle("#contact-nav", "highlight") // add class toggle
+  new ScrollMagic.Scene({
+    triggerElement: '#contact',
+  })
+    .setClassToggle('#contact-nav', 'highlight') // add class toggle
     .addTo(controller);
-
 });
